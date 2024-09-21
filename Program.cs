710b09b5781e8620f57afa88d85a48f7657f4334
rdanilowicz <rjdanilowicz@gmail.com>
2024-09-21 12:43:36 -0400

    Finshed the calculator.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1;
+            int num2;
+
+            int result;
+
+            string answer;
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+            Console.WriteLine("Please enter your first number.");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number.");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operating would you like to do?");
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplocation or any other key for division.");
+
+            answer = Console.ReadLine();
+
+            if (answer == "a")
+            {
+                result = num1 + num2;
+            }
+            else if (answer == "s")
+            {
+                result = num1 - num2;
+            }
+            else if (answer == "m")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.WriteLine("Thank you for using the calculator program!");
+
+            Console.ReadKey();
         }
     }
 }
